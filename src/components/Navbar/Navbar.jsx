bc56d1a93b0ce478d4a9ed514febf77bd38359a3
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-25 13:36:08 -0300

    navbar tiene la opcion de ver la cantidad.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,8 @@ import Logo from "@assets/Navbar/logo.png";
 import Carrito from "@assets/Navbar/cart.svg";
 import Search from "@assets/Navbar/search.svg";
 import Menu from "@assets/Navbar/menu.svg";
-import useCartShop from "@hooks/useCartShop";
 
-function Navbar({toggle, navigation}) {
-  const {amountProducts} = useCartShop();
+function Navbar({toggle, navigation, cantidad}) {
   return (
     <div className="w-full shadow-lg">
         <div className="relative flex h-[80px] w-full justify-center bg-white">
@@ -31,7 +29,7 @@ function Navbar({toggle, navigation}) {
             <div className="absolute right-4 h-full w-auto flex justify-center items-center">
                 <div className="size-[60px] flex justify-center items-center relative cursor-pointer" onClick={toggle}>
                   <img src={Carrito} alt="carrito" className="size-[35px]"/>
-                  <span className="bg-orange-500 size-5 rounded-[50%] text-xs text-white flex justify-center items-center absolute right-[5px] top-[5px]">{amountProducts}</span>
+                  <span className="bg-orange-500 size-5 rounded-[50%] text-xs text-white flex justify-center items-center absolute right-[5px] top-[5px]">{cantidad}</span>
                 </div>
             </div>
         </div>
