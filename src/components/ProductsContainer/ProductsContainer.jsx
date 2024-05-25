e8b084789f92449ca7cd58ab1e5fc53bb6f7f681
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-25 13:36:40 -0300

    funcion agregar al carrito.

--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -1,14 +1,14 @@
 import CardProduct from "@components/CardProduct/CardProduct";
 import useProducts from "./useProducts";
 
-function ProductsContainer({urlProducts}) {
+function ProductsContainer({urlProducts, addProduct}) {
   const customStyles = { gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))', };
   const products = useProducts(urlProducts);
   //console.log("url: " + urlProducts);
   return (
     <div className="pt-4">
       <div className="w-auto grid gap-6" style={customStyles}>
-        {products.map((product, index) => (<CardProduct key={index} product={product}/>))}
+        {products.map((product, index) => (<CardProduct key={index} product={product} addProduct={addProduct}/>))}
       </div>
     </div>
 
