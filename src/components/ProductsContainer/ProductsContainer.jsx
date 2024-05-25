2eb3a38d6349a82a9c56b5763cf91fa693b73de8
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-24 23:07:33 -0300

    no muestra la url

--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -4,7 +4,7 @@ import useProducts from "./useProducts";
 function ProductsContainer({urlProducts}) {
   const customStyles = { gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))', };
   const products = useProducts(urlProducts);
-  console.log("url: " + urlProducts);
+  //console.log("url: " + urlProducts);
   return (
     <div className="pt-4">
       <div className="w-auto grid gap-6" style={customStyles}>
