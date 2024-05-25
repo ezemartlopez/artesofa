b0605197d66b5f15d56a9ee59808eb1fe341dc0a
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-24 21:01:30 -0300

    creando breadcramb

---
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,29 @@
+
+function obtenerLinks(listaString){
+    if(!listaString) return [];
+    const nueva = listaString;
+    const url = window.location.origin + "/";
+    const resultado = []
+    const urls = window.location.pathname.split("/");
+    nueva.forEach(element => {
+        const objeto = {text:element, url:null}
+        if(element !== ">"){
+            objeto.url = url + urls.shift();
+        }
+        resultado.push(objeto);
+    });
+    resultado[0].text = "Inicio";
+    return resultado;
+}
+function Breadcrumb({links}) {
+    const lista = obtenerLinks(links);
+  return (
+    <div className="flex gap-1 text-white">
+        {
+         lista.map(obj => (obj.url !== null? <a href={obj.url}>{obj.text}</a>:<span>{obj.text}</span>))
+        }
+    </div>
+  )
+}
+
+export default Breadcrumb
