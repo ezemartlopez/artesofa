1fff67c16bbfc8be8b3fc367a4eea873abd8e58e
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-24 23:08:05 -0300

    modificacion de la funcion agregar

--- src/hooks/useCartShop.jsx
+++ src/hooks/useCartShop.jsx
@@ -7,19 +7,33 @@ function useCartShop() {
         return currentList;
     }
     const createProdutcCart = (id, name, url_image, price) =>{
-        return {id, name, url_image, price};
+        let count = 1;
+        return {id, name, url_image, price, count};
     }
     const [cartList, setCartList] = useState(chargeCartShop());
-    const handleAddStorageCartList = (id, name, url_image, price) =>{
-        const producto = createProdutcCart(id, name, url_image, price);
-        setCartList((lista) => [...lista, producto]);
-    }
+
+    const handleAddStorageCartList = (id, name, url_image, price) => {
+        const existingProduct = cartList.find((product) => product.id === id);
+      
+        if (existingProduct) {
+          setCartList((prevList) =>
+            prevList.map((product) =>
+              product.id === id
+                ? { ...product, count: product.count + 1 }
+                : product
+            )
+          );
+        } else {
+          const newProduct = createProdutcCart(id, name, url_image, price);
+          setCartList((prevList) => [...prevList, newProduct]);
+        }
+    };
 
     useEffect(() => {
         //cargando info en la storage
         localStorage.setItem(CART_STORAGE_NAME, JSON.stringify(cartList));
     }, [cartList])
-    return {cartList, addProduct: handleAddStorageCartList};
+    return {cartList, addProduct: handleAddStorageCartList, amountProducts: cartList.length};
 }
 
 export default useCartShop;
