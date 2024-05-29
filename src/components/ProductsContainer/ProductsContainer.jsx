065659bd51c1000dc1db4f3fc602eed3cee68e68
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-28 23:16:27 -0300

    products modificacion de estilos

--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -1,13 +1,14 @@
 import CardProduct from "@components/CardProduct/CardProduct";
-import useProducts from "./useProducts";
+import useProducts from "@hooks/useProducts";
+import './productsContainer.css';
 
 function ProductsContainer({urlProducts, addProduct}) {
-  const customStyles = { gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))', };
+  //const customStyles = { gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', };
   const products = useProducts(urlProducts);
   //console.log("url: " + urlProducts);
   return (
     <div className="pt-4">
-      <div className="w-auto grid gap-6" style={customStyles}>
+      <div className="w-full product-grid">
         {products.map((product, index) => (<CardProduct key={index} product={product} addProduct={addProduct}/>))}
       </div>
     </div>
