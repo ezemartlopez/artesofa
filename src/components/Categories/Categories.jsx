9da5f313c03dd8f107d1b00845c34e1d598d573a
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-05-29 20:12:45 -0300

    bug en mobile corregido

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -6,27 +6,27 @@ import Rinconeros from "@assets/Categories/rinconeros.png";
 function Categories() {
   return (
 <div className="xl:h-[750px] sm:h-[1500px] h-[1300px] w-full">
-  <div className="grid h-full w-full grid-cols-12 gap-8">
+  <div className="grid h-full w-full xl:grid-cols-12 grid-cols-1 gap-8">
 
-    <div className="h-full xl:col-span-5 col-span-12 ">
-      <a href="/sofas/" target="_blank">
-        <div className="w-full h-full relative cursor-pointer group overflow-hidden">
+    <div className="h-full xl:col-span-5 col-span-1">
+      <a href="/sofas/" target="_blank" className="w-full">
+        <div className=" h-full relative cursor-pointer group overflow-hidden">
           <img src={Sofas} alt="categorie_sofa" className="absolute w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"/>
           <div className="absolute z-10 w-full h-full flex justify-center items-center bg-slate-500 bg-opacity-25 group-hover:bg-opacity-40 group-hover:bg-slate-700 transition-all duration-500">
-            <span className="text-[70px] text-white font-rubik font-bold z-10">SOFAS</span>
+            <span className="sm:text-[70px] text-[50px] text-white font-rubik font-bold z-10">SOFAS</span>
           </div>
         </div>
       </a>
     </div>
 
-    <div className="xl:col-span-7 col-span-12 grid grid-rows-2 gap-8">
+    <div className="xl:col-span-7 col-span-1 grid grid-rows-2 gap-8">
 
       <div>
         <a href="/esquineros/" target="_blank">
           <div className="w-full h-full relative cursor-pointer group overflow-hidden">
             <img src={Esquineros} alt="categorie_sofa" className="absolute w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"/>
             <div className="absolute z-10 w-full h-full flex justify-center items-center bg-slate-500 bg-opacity-25 group-hover:bg-opacity-40 group-hover:bg-slate-700 transition-all duration-500">
-              <span className="sm:text-[70px] text-[50px] text-white font-rubik font-bold z-10">ESQUINEROS</span>
+              <span className="sm:text-[70px] text-[40px] text-white font-rubik font-bold z-10">ESQUINEROS</span>
             </div>
           </div>
         </a>
@@ -39,7 +39,7 @@ function Categories() {
             <div className="w-full h-full relative cursor-pointer group overflow-hidden">
               <img src={Muebles} alt="categorie_sofa" className="absolute w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"/>
               <div className="absolute z-10 w-full h-full flex justify-center items-center bg-slate-500 bg-opacity-25 group-hover:bg-opacity-40 group-hover:bg-slate-700 transition-all duration-500">
-                <span className="md:text-[50px] sm:text-[40px] text-[50px] text-white font-rubik font-bold z-10">MUEBLES</span>
+                <span className="md:text-[50px] sm:text-[45px] text-[40px] text-white font-rubik font-bold z-10">MUEBLES</span>
               </div>
             </div>
           </a>
@@ -50,7 +50,7 @@ function Categories() {
             <div className="w-full h-full relative cursor-pointer group overflow-hidden">
               <img src={Rinconeros} alt="categorie_sofa" className="absolute w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"/>
               <div className="absolute z-10 w-full h-full flex justify-center items-center bg-slate-500 bg-opacity-25 group-hover:bg-opacity-40 group-hover:bg-slate-700 transition-all duration-500">
-                <span className="md:text-[50px] sm:text-[40px] text-[50px] text-white font-rubik font-bold z-10">RINCONEROS</span>
+                <span className="md:text-[50px] sm:text-[45px] text-[40px] text-white font-rubik font-bold z-10">RINCONEROS</span>
               </div>
             </div>
           </a>
