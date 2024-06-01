112196c16745bbf37141bec3a22540269edc1d30
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-06-01 00:07:00 -0300

    modificacion para el tamanio en mobile.

--- src/components/titlePage/TitlePage.jsx
+++ src/components/titlePage/TitlePage.jsx
@@ -2,7 +2,7 @@ import Breadcrumb from "../Breadcrumb/Breadcrumb"
 
 function TitlePage({titulo, links}) {
   return (
-    <div className="my-2 bg-sky-600 w-full h-[150px] flex flex-col items-center justify-center gap-2">
+    <div className="my-2 bg-sky-600 w-full sm:h-[150px] h-[100px] rounded-md flex flex-col items-center justify-center sm:gap-6 gap-2">
         <span className="text-white sm:text-6xl text-4xl font-bold">{titulo}</span>
         <Breadcrumb links={links}/>
     </div>
