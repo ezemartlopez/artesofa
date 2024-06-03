04d7ab31dbcb6e6e941ddbf62326433bc535594a
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2024-06-03 15:17:09 -0300

    modificacion si los slides es de len = 1.

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -9,15 +9,19 @@ export default function Carousel({
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
+    if(slides.length > 1){
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
+    setCurrentIndex(newIndex);      
+    }
   };
 
   const nextSlide = () => {
     //const isLastSlide = currentIndex === slides.length - 1;
     //const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex((currentIndex === slides.length - 1) ? 0 : currentIndex + 1);
+    if(slides.length > 1){
+      setCurrentIndex((currentIndex === slides.length - 1) ? 0 : currentIndex + 1);
+    }
   };
 
   const goToSlide = (slideIndex) => {
@@ -36,16 +40,16 @@ export default function Carousel({
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-slate-500/50 text-white cursor-pointer'>
         <div onClick={prevSlide} size={30}>
-          <svg className="size-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor"  stroke-width="2" d="M5 1 1 5l4 4"/>
+          <svg className="size-4 stroke-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor"  d="M5 1 1 5l4 4"/>
           </svg> 
         </div>
       </div>
       {/* Right Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-slate-500/50 text-white cursor-pointer'>
         <div onClick={nextSlide} size={30}>
-          <svg className="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor"  stroke-width="2" d="m1 9 4-4-4-4"/>
+          <svg className="w-4 h-4 stroke-2 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor" d="m1 9 4-4-4-4"/>
           </svg> 
         </div>
       </div>
